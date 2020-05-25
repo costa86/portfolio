2705cb367fa536fc03c8a338fca12876845ae21a
costa86 <costa86@zoho.com>
2020-05-25 10:36:13 +0100

    Updates

---
+++ precache-manifest.10ef4ab048bc0339a56e36e301a7b61f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8a8dc7a19525caa42c92f8258d6790",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06c614a0f0ed05c286cf",
+    "url": "/portfolio/static/css/main.94ba0f8e.chunk.css"
+  },
+  {
+    "revision": "8883e6e1d62570db4c1a",
+    "url": "/portfolio/static/js/2.1548317b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1548317b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c614a0f0ed05c286cf",
+    "url": "/portfolio/static/js/main.c189f51d.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
