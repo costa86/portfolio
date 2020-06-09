846471d99070864bb05a2dd372d44a2ce52f17ae
costa86 <costa86@zoho.com>
2020-06-09 13:54:58 +0100

    v 2

---
+++ contacts.js
@@ -0,0 +1,40 @@
+export const phone = {
+    title: "(+351) 920 438 006",
+    img: "https://img.icons8.com/color/48/000000/phone.png",
+    link: "",
+    text: "Call me"
+
+}
+
+const skype = {
+    title: "(+351) 920 438 006",
+    img: "https://img.icons8.com/color/48/000000/skype.png",
+    link: "skype:lourenzo86@outlook.com?chat",
+    text: "Chat with me on Skype"
+
+}
+
+const email = {
+    title: "costa86@zoho.com",
+    img: "https://img.icons8.com/color/48/000000/email.png",
+    link: "mailto:costa86@zoho.com/",
+    text: "Send me an email"
+
+}
+
+const linkedin = {
+    title: "linkedin.com/in/costa86",
+    img: "https://img.icons8.com/color/48/000000/linkedin.png",
+    link: "https://www.linkedin.com/in/costa86/",
+    text: "Connect with me on LinkedIn"
+
+}
+
+const github = {
+    title: "github.com/costa86",
+    img: "https://img.icons8.com/material-outlined/48/000000/github.png",
+    link: "https://github.com/costa86",
+    text: "Check out my GitHub repository"
+
+}
+
--- src/components/Courses.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Courses() {
-    return (
-        <>
-            <h1>Courses</h1>
-        </>
-    );
-}
